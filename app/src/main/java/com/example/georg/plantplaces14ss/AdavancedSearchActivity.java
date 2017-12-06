9d37b5b3511c22ab3b40390d76fe7e7ec2a23716
eldok11 <greko12345@hotmail.com>
2017-12-06 20:30:34 +0100

    Noch eine Activity erstellt plantresultsactivity, jedoch ohne xml layout, erbt von ListActivity, um liste mit resultaten zu zeigen in adavancedserachactivity durch button ausgelöst, nicht vergessen neue activity in manifest datei zu ergänzen

--- app/src/main/java/com/example/georg/plantplaces14ss/AdavancedSearchActivity.java
+++ app/src/main/java/com/example/georg/plantplaces14ss/AdavancedSearchActivity.java
@@ -1,9 +1,11 @@
 package com.example.georg.plantplaces14ss;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Georg on 06.12.2017.
@@ -17,4 +19,14 @@ public class AdavancedSearchActivity extends AppCompatActivity {
         setContentView(R.layout.advancedsearch);
         //layout mit diesem activity verbinden
     }
+
+
+      // nicht vergessen im manifest aktivity anzugeben
+    public void searchForPlants(View view){
+        Intent plantResultsIntent=new Intent(this,PlantResultsActivity.class);
+        startActivity(plantResultsIntent);
+
+        }
+
+
 }
---
+++ app/src/main/java/com/example/georg/plantplaces14ss/PlantResultsActivity.java
@@ -0,0 +1,18 @@
+package com.example.georg.plantplaces14ss;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Georg on 06.12.2017.
+ */
+
+public class PlantResultsActivity extends ListActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
