37d59550f0ae193005372ff06f02f94a363330dc
eldok11 <greko12345@hotmail.com>
2017-12-07 15:11:39 +0100

    Daten übergtragen von adavancedSearchactivity zu plantresultsactivity

--- app/src/main/java/com/example/georg/plantplaces14ss/AdavancedSearchActivity.java
+++ app/src/main/java/com/example/georg/plantplaces14ss/AdavancedSearchActivity.java
@@ -6,6 +6,7 @@ import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 
 /**
  * Created by Georg on 06.12.2017.
@@ -13,17 +14,30 @@ import android.view.View;
 
 public class AdavancedSearchActivity extends AppCompatActivity {
 
+    private AutoCompleteTextView actPlantName;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.advancedsearch);
         //layout mit diesem activity verbinden
+
+        //
+        actPlantName = findViewById(R.id.actPlantName);
     }
 
 
       // nicht vergessen im manifest aktivity anzugeben
     public void searchForPlants(View view){
         Intent plantResultsIntent=new Intent(this,PlantResultsActivity.class);
+
+        //daten abspeichern, die vom user im edittext eingegeben werude.
+        String searchPlantName = actPlantName.getText().toString();
+
+        //daten an nächste aktivity geben
+        plantResultsIntent.putExtra("SEARCH_PLANT_NAME",searchPlantName);
+
         startActivity(plantResultsIntent);
 
         }
--- app/src/main/java/com/example/georg/plantplaces14ss/PlantResultsActivity.java
+++ app/src/main/java/com/example/georg/plantplaces14ss/PlantResultsActivity.java
@@ -17,25 +17,44 @@ public class PlantResultsActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //bekomme daten von adavancedserachactivity, welches bei edittext eingegeben wurde.
+        String searchTerm = getIntent().getStringExtra("SEARCH_PLANT_NAME");
 
-        Plant redbud=new Plant();
-        redbud.setCommon("Redbud");
-        redbud.setGenus("Cercis");
-        redbud.setSpecies("candensis");
 
-        Plant pawpaw=new Plant();
-        pawpaw.setCommon("pawpaw");
-        pawpaw.setGenus("asimin");
-        pawpaw.setSpecies("triloba");
+        ArrayList<Plant> allPlants = new ArrayList<Plant>();
 
-        ArrayList<Plant>allPlants=new ArrayList<Plant>();
+        //wenn redbud gleich searchTerm variable, dann hinzufügen
+        if (searchTerm.equalsIgnoreCase("Redbud")){
 
-        allPlants.add(redbud);
-        allPlants.add(pawpaw);
+            Plant redbud = new Plant();
+            redbud.setCommon("Redbud");
+            redbud.setGenus("Cercis");
+            redbud.setSpecies("candensis");
+
+            allPlants.add(redbud);
+    }
+        //wenn irgendwas pawpaw, pawpaw also enthalten ist, der liste hinzufügen
+        if(searchTerm.contains("pawpaw")) {
+            Plant pawpaw = new Plant();
+            pawpaw.setCommon("pawpaw");
+            pawpaw.setGenus("asimin");
+            pawpaw.setSpecies("triloba");
+
+            allPlants.add(pawpaw);
+        }
+
+        if(allPlants.size()==0){
+            Plant empty=new Plant();
+            empty.setCommon("no plants match your result, please try again");
+            empty.setGenus("");
+            empty.setSpecies("");
+            allPlants.add(empty);
+        }
         //nimmt die arraylist auf und zeigt es im display für den user an, erbt von listactivity
         ArrayAdapter<Plant> plantAdapter = new ArrayAdapter<Plant>(this,android.R.layout.simple_list_item_1, allPlants);
         setListAdapter(plantAdapter);
         //objekt wird so angezeigt, wie wir in der tostring methode angegeben haben, die hier aufgerufen wird
+        //auch listview objekt möglich, dass dann listView.setAdapter(plantAdapter); benutzt benutzt werden kann
     }
 
 
